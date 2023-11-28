93f7baa27535230b9ba2af0b75ee6d58466b3148
Dilnura Rizayeva <didi.riri2004@mail.ru>
2023-11-28 09:42:29 +0600

    Initial Commit

---
+++ HW-21- Rizayeva Dilnura/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW-21- Rizayeva Dilnura
+//
+//  Created by Dilnura Rizaeva on 28.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
